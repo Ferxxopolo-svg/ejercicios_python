feb23beaf9d102147af157764dd78adf701fda8e
Fernando_Polo <polofernando54@gmail.com>
2026-01-29 09:19:46 -0600

    Siguientes condicionales

---
+++ condicionales/ejercicio_04.py
@@ -0,0 +1,7 @@
+dividendo = int(input("Dime el primer numero: "))
+divisor = int(input("Dime el segundo numero: "))
+
+if divisor == 0:
+	print("No puedes dividir por 0")
+else:
+	print("La division es: ", dividendo / divisor)
---
+++ condicionales/ejercicio_05.py
@@ -0,0 +1,7 @@
+usuario = input('Ingresa tu usuario: ')
+contraseña = input('Ingresa tu contraseña: ')
+
+if usuario == "pepe" and contraseña == "asdasd":
+	print("Has Entrado Al Sistema")
+else:
+	print("usuario y contraseña son incorrectos")
